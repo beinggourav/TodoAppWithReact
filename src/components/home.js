cf0317e6833b230b30ac7f549ab4bfa6b88bb8e3
Gourav <82900z24071@gmail.com>
2021-07-16 16:59:00 +0530

    Added Home UI

---
+++ src/components/home.js
@@ -0,0 +1,33 @@
+const Home = () => {
+    return (
+        <div className="cards">
+            <div className="card UrgentImportant">
+                <h2>Urgent and Important</h2>
+                <p>Here is your Urgent and Important tasks</p>
+                <p>Click to have a look</p>
+                <h3>Priority: Highest</h3>
+            </div>
+            <div className="card NotUrgentImportant">
+                <h2>Not Urgent and Important</h2>
+                <p>Here is your Not Urgent and Important tasks</p>
+                <p>Click to have a look</p>
+                <h3>Priority: High</h3>
+            </div>
+            <div className="card UrgentNotImportant">
+                <h2>Urgent and Not Important</h2>
+                <p>Here is your Urgent and Not Important tasks</p>
+                <p>Click to have a look</p>
+                <h3>Priority: Medium</h3>
+            </div>
+            <div className="card NotUrgentNotImportant">
+                <h2>Not Urgent and Not Important</h2>
+                <p>Here is your neither Urgent nor Important tasks</p>
+                <p>Click to have a look</p>
+                <h3>Priority: Low</h3>
+            </div>
+        </div>
+        
+    );
+}
+ 
+export default Home;
---
+++ src/components/navbar.js
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <h1>To-Do App with Eisenhower matrix implementation</h1>
+            <h2><a href="/">Home</a></h2>
+        </div>
+    );
+}
+ 
+export default Navbar;
